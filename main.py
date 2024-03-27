cc15db79a9708ad17d2cacbf2dfefff0cbdd8c16
Mslaise <lilylake415246@gmail.com>
2024-03-27 13:31:19 -0500

    the clock now runs on a separate process and communicates with the GFI by sending it data every real-time second.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from Classes.gfi import Gfi
 import Code.print_dumps as print_dumps
 from Code.terminal_loop import TerminalLoop
 from Code.init_cocos import InitCocos
+from Code.clock_loop import ClockLoop
 
 from multiprocessing import Process, Lock, Pipe
 
@@ -23,11 +24,14 @@ if __name__ == "__main__":
     lock = Lock()
     act_handle.Lock = lock
     cocosIn, cocosOut = Pipe()
+    clockIn, clockOut = Pipe()
     
     #Actual creation of he cocos thread
     cocosProcess = Process(target=InitCocos,args=(lock,cocosOut))
-    
+    clockProcess = Process(target=ClockLoop,args=(lock,cocosIn))
     lock.acquire()
+    
+    clockProcess.start()
     while act_handle.Go and not act_handle.Gfi:
         #there's probably a better way to handle this but honestly idgaf right now
         TerminalLoop(lock, cocosIn)
